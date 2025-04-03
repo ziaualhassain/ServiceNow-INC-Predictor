fb6d6c9c5e4dc976914b6c4c9dd8f37e5a5f7703
Zia <ziaualhassain@gmail.com>
2025-04-03 18:22:28 +0530

    Updated .gitignore

--- FrontEnd/node_modules/.vite/deps/react-dom.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-Z5QNEGRH.js";
-import "./chunk-NG4UJB3X.js";
-import "./chunk-HKJ2B2AA.js";
-export default require_react_dom();
--- FrontEnd/node_modules/.vite/deps/react.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react
-} from "./chunk-NG4UJB3X.js";
-import "./chunk-HKJ2B2AA.js";
-export default require_react();
